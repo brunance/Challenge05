3e12900a7047e95d2a901bf9fbdec6116fee6e54
Bruno <bruno_french@outlook.com>
2022-11-07 08:15:43 -0400

    chore: add core files

---
+++ Challenge05/Challenge05App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge05App.swift
+//  Challenge05
+//
+//  Created by Bruno França do Prado on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Challenge05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
