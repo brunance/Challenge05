6938b7dccc5cd41ed5b80f372172ce6996effc6a
Anna Milani <aavm.snf19@uea.edu.br>
2022-11-10 23:53:02 -0400

    feat: adicionando animação na contagem regressiva

--- Challenge05/Views/CountDownView.swift
+++ Challenge05/Views/CountDownView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CountDownView: View {
     @State var countDownTimer = 5
     @State var timerRunning = true
+    @State var scale: CGFloat = 1
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
@@ -22,10 +23,20 @@ struct CountDownView: View {
                     Image(systemName: "circle.fill")
                         .font(.system(size: 200))
                         .foregroundColor(Color("TextColorNames"))
+                        .scaleEffect(scale)
+                        .opacity(Double(2 - scale))
+                        .onAppear {
+                            let baseAnimation = Animation.easeInOut(duration: 0.9)
+                            let repeated = baseAnimation.repeatForever(autoreverses: false)
+                            withAnimation(repeated) {
+                                scale += 1
+                            }
+                        }
 
                     Text("\(countDownTimer)")
                         .onReceive(timer) { _ in
                             if countDownTimer > 0 && timerRunning {
+                                playSound(sound: "countdown", type: "wav")
                                 countDownTimer -= 1
                             } else {
                                 timerRunning = false
@@ -33,6 +44,9 @@ struct CountDownView: View {
                         }
                         .font(.custom("RubikBubbles-Regular", size: 100))
                         .foregroundColor(Color("BackgroundColor1"))
+                        .frame(width: 200, height: 200)
+                        .background(Color("TextColorNames"))
+                        .clipShape(Circle())
                 }
             }
         }
