67616c83346c9164d794e9be30543ce06c434e5c
Bruno <bruno_french@outlook.com>
2022-11-16 09:07:23 -0400

    chore: adicionar projeto watch no escobo

---
+++ orkhestraWatch Watch App/orkhestraWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  orkhestraWatchApp.swift
+//  orkhestraWatch Watch App
+//
+//  Created by Bruno França do Prado on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct orkhestraWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
