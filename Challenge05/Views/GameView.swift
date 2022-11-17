313025a85e1e950815910ec524bfcfda832b1a0e
Anna Milani <aavm.snf19@uea.edu.br>
2022-11-17 00:19:22 -0400

    feat: Adicionar ação no popup de sair da estação

--- Challenge05/Views/GameView.swift
+++ Challenge05/Views/GameView.swift
@@ -26,6 +26,7 @@ struct GameView: View {
     @State var userChoices = [CardModel]()
     @State private var showAlert = false
     @State var isGameView = false
+    @State private var showingHistoryView = false
     var body: some View {
 
         let currentHistory = historyList[hvm.historyCount]
@@ -33,120 +34,135 @@ struct GameView: View {
         if matchedCards.count == cards.count {
             CountDownView(isGameView: $isGameView)
         } else {
-            GeometryReader { geometry in
-                ZStack {
-                    Color("Primaria1").ignoresSafeArea()
-                    Image("\(currentHistory.name)Padrao")
-                        .resizable()
-                    VStack {
-                        VStack {
-                            Button("Sair aqui") {
-                                showAlert = true
-                            }
-                            .alert(isPresented: $showAlert) {
-                                Alert(
-                                    title: Text("Tem certeza de que quer sair?"),
-                                    message: Text("A sessão de música não será salva."),
-                                    primaryButton: .default(
-                                        Text("Continuar")
-                                    ),
-                                    secondaryButton: .destructive(
-                                        Text("Sair")
-                                    )
-                                )
-                            }
-
-                            VStack {
-                                ForEach(0...currentHistory.titleList.count - 1, id: \.self) { num in
-                                    Text(currentHistory.titleList[num])
-                                        // swiftlint:disable:next line_length
-                                        .font(.custom("RubikBubbles-Regular", size: CGFloat(currentHistory.sizeTitleList[num])))
-                                        .foregroundColor(Color("TitleHistory"))
-                                }
-                            }
-                        }
-
-                        Text("Combine os sons para desbloquear uma melodia")
-                            .font(.system(size: 16))
-                            .frame(maxWidth: .infinity, alignment: .top)
-                            .bold()
-                            .foregroundColor(Color("TitleHistory"))
-
-                        Text("0/6")
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                            .font(.system(size: 16))
-                            .foregroundColor(Color("TitleHistory"))
-
+            NavigationView {
+                GeometryReader { geometry in
+                    ZStack {
+                        Color("Primaria1").ignoresSafeArea()
+                        Image("\(currentHistory.name)Padrao")
+                            .resizable()
                         VStack {
                             ZStack {
-                                RoundedRectangle(cornerRadius: 8, style: .continuous)
-                                    .foregroundColor(Color("TitleHistory"))
-                                    .frame(width: 350, height: 60)
-                                HStack {
-                                    VStack {
-                                        Image("Pandeiro")
-                                            .font(.system(size: 40))
-                                            .foregroundColor(.white)
+                                Button(action: {
+                                    showAlert = true
+                                }, label: {
+                                    ZStack {
+                                        Image(systemName: "xmark.circle.fill")
+                                            .frame(alignment: .trailing)
+                                            .font(.system(size: 37))
+                                            .foregroundStyle(Color("CircleCount"))
                                     }
-                                    VStack {
-                                        Image("Chocalho")
-                                            .font(.system(size: 40))
-                                            .foregroundColor(.white)
-                                    }
-                                    .padding(.init(top: 0, leading: 10, bottom: 0, trailing: 10))
-                                    VStack {
-                                        Image("Flauta")
-                                            .font(.system(size: 40))
-                                            .foregroundColor(.white)
-                                    }
-                                    VStack {
-                                        Image("Saxofone")
-                                            .font(.system(size: 40))
-                                            .foregroundColor(.white)
-                                    }
-                                    VStack {
-                                        Image("Violao")
-                                            .font(.system(size: 40))
-                                            .foregroundColor(.white)
-                                    }
-                                    VStack {
-                                        Image("Piano")
-                                            .font(.system(size: 40))
-                                            .foregroundColor(.white)
-                                    }
-                                    .padding(.init(top: 0, leading: 10, bottom: 0, trailing: 0))
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                                })
+                                .alert(isPresented: $showAlert) {
+                                    Alert(
+                                        title: Text("Por favor, espere!"),
+                                        message: Text("Você tem certeza que deseja abandonar a dinâmica agora?"),
+                                        primaryButton: .default(
+                                            Text("Continuar")
+                                        ),
+                                        secondaryButton: .destructive(
+                                            Text("Sair"),
+                                            action: {
+                                                showingHistoryView = true
+                                            }
+                                        )
+                                    )
                                 }
-                                .padding(.init(top: 5, leading: 0, bottom: 5, trailing: 0))
-                            }
 
-                        }
-                        .padding(.init(top: 0, leading: 0, bottom: 20, trailing: 0))
-                        
-                        LazyVGrid(columns: threeColumnGrid, spacing: 10) {
-                            ForEach(cards) { card in
-                                CardView(card: card,
-                                         matchedCards: $matchedCards,
-                                         userChoices: $userChoices
-                                )
+                                VStack {
+                                    ForEach(0...currentHistory.titleList.count - 1, id: \.self) { num in
+                                        Text(currentHistory.titleList[num])
+                                        // swiftlint:disable:next line_length
+                                            .font(.custom("RubikBubbles-Regular", size: CGFloat(currentHistory.sizeTitleList[num])))
+                                            .foregroundColor(Color("TitleHistory"))
+                                    }
+                                }
+                            }.padding(.init(top: 0, leading: 0, bottom: 10, trailing: 0))
+
+                            Text("Combine os sons para desbloquear uma melodia")
+                                .font(.system(size: 16))
+                                .bold()
+                                .foregroundColor(Color("TitleHistory"))
+                                .multilineTextAlignment(.center)
+
+                            Text("0/6")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                                .font(.system(size: 16))
+                                .foregroundColor(Color("TitleHistory"))
+
+                            VStack {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                                        .foregroundColor(Color("TitleHistory"))
+                                        .frame(width: 350, height: 60)
+                                    HStack {
+                                        VStack {
+                                            Image("Pandeiro")
+                                                .font(.system(size: 40))
+                                                .foregroundColor(.white)
+                                        }
+                                        VStack {
+                                            Image("Chocalho")
+                                                .font(.system(size: 40))
+                                                .foregroundColor(.white)
+                                        }
+                                        .padding(.init(top: 0, leading: 10, bottom: 0, trailing: 10))
+                                        VStack {
+                                            Image("Flauta")
+                                                .font(.system(size: 40))
+                                                .foregroundColor(.white)
+                                        }
+                                        VStack {
+                                            Image("Saxofone")
+                                                .font(.system(size: 40))
+                                                .foregroundColor(.white)
+                                        }
+                                        VStack {
+                                            Image("Violao")
+                                                .font(.system(size: 40))
+                                                .foregroundColor(.white)
+                                        }
+                                        VStack {
+                                            Image("Piano")
+                                                .font(.system(size: 40))
+                                                .foregroundColor(.white)
+                                        }
+                                        .padding(.init(top: 0, leading: 10, bottom: 0, trailing: 0))
+                                    }
+                                    .padding(.init(top: 5, leading: 0, bottom: 5, trailing: 0))
+                                }
+
                             }
+                            .padding(.init(top: 0, leading: 0, bottom: 20, trailing: 0))
+
+                            LazyVGrid(columns: threeColumnGrid, spacing: 10) {
+                                ForEach(cards) { card in
+                                    CardView(card: card,
+                                             matchedCards: $matchedCards,
+                                             userChoices: $userChoices
+                                    )
+                                }
+                            }
+                            //                        VStack {
+                            //                            Text("Match these cards to win: ")
+                            //                                .font(.system(size: 28))
+                            //                                .padding()
+                            //                            LazyVGrid(columns: sixColumnGrid, spacing: 5) {
+                            //                                ForEach(cardValues, id: \.self) { cardValue in
+                            //                                    if !matchedCards.contains(where: {$0.text == cardValue}) {
+                            //                                        Text(cardValue)
+                            //                                            .font(.system(size: 50))
+                            //                                    }
+                            //                                }
+                            //                            }
+                            //                        }
                         }
-                        //                        VStack {
-                        //                            Text("Match these cards to win: ")
-                        //                                .font(.system(size: 28))
-                        //                                .padding()
-                        //                            LazyVGrid(columns: sixColumnGrid, spacing: 5) {
-                        //                                ForEach(cardValues, id: \.self) { cardValue in
-                        //                                    if !matchedCards.contains(where: {$0.text == cardValue}) {
-                        //                                        Text(cardValue)
-                        //                                            .font(.system(size: 50))
-                        //                                    }
-                        //                                }
-                        //                            }
-                        //                        }
+                        .padding()
                     }
-                    .padding()
+                    .ignoresSafeArea()
+
+                    NavigationLink(destination: HistoryView().navigationBarBackButtonHidden(true), isActive: $showingHistoryView) {}
                 }
-                .ignoresSafeArea()
             }
         }
     }
