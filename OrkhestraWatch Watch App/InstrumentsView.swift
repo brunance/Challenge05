ed98aca55942518c5f62b078652211753f1765b7
Bruno <bruno_french@outlook.com>
2022-11-17 09:34:42 -0400

    feat: adicionar tela de instrumentos e seus assets

--- OrkhestraWatch Watch App/InstrumentsView.swift
+++ OrkhestraWatch Watch App/InstrumentsView.swift
@@ -9,18 +9,75 @@ import SwiftUI
 struct InstrumentsView: View {
     var body: some View {
         VStack {
-            Text("Escolha um instrumento para ouvir!")
-                .font(.system(size: 14))
-                .frame(maxWidth: .infinity, alignment: .center)
-            ScrollView (.horizontal, showsIndicators: false) {
-                HStack {
-                    Text("Oi")
-                        .frame(width: 100, height: 100)
-                    Text("Olá")
-                        .frame(width: 100, height: 100)
+            List{
+                VStack(alignment: .center, spacing: 5) {
+                    Image("Chocalho").padding(.top, 5)
+                    Text("Chocalho").bold().padding(.top, 5)
+                    Button("**ESCOLHER**") {
+                        print("Begin 1")
+                    }
+                    .foregroundColor(.red).padding(.bottom, 5)
                 }
-                .frame(maxWidth: .infinity)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding()
+                
+                VStack(alignment: .center, spacing: 5) {
+                    Image("Flauta").padding(.top, 5)
+                    Text("Flauta").bold().padding(.top, 5)
+                    Button("**ESCOLHER**") {
+                        print("Begin 2")
+                    }
+                    .foregroundColor(.red).padding(.bottom, 5)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding()
+                
+                VStack(alignment: .center, spacing: 5) {
+                    Image("Pandeiro").padding(.top, 5)
+                    Text("Pandeiro").bold().padding(.top, 5)
+                    Button("**ESCOLHER**") {
+                        print("Begin 3")
+                    }
+                    .foregroundColor(.red).padding(.bottom, 5)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding()
+                
+                VStack(alignment: .center, spacing: 5) {
+                    Image("Piano").padding(.top, 5)
+                    Text("Piano").bold().padding(.top, 5)
+                    Button("**ESCOLHER**") {
+                        print("Begin 4")
+                    }
+                    .foregroundColor(.red).padding(.bottom, 5)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding()
+                
+                VStack(alignment: .center, spacing: 5) {
+                    Image("Saxofone").padding(.top, 5)
+                    Text("Saxofone").bold().padding(.top, 5)
+                    Button("**ESCOLHER**") {
+                        print("Begin 5")
+                    }
+                    .foregroundColor(.red).padding(.bottom, 5)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding()
+                
+                VStack(alignment: .center, spacing: 5) {
+                    Image("Violao").padding(.top, 5)
+                    Text("Violão").bold().padding(.top, 5)
+                    Button("**ESCOLHER**") {
+                        print("Begin 6")
+                    }
+                    .foregroundColor(.red).padding(.bottom, 5)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding()
+                
             }
+            .listStyle(CarouselListStyle())
         }
         .padding()
     }
